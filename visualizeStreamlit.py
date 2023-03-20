53855fbfc126560380fa8637ea5d69bf718c17e9
sankalpbhatia2003 <119621204+sankalpbhatia2003@users.noreply.github.com>
2023-03-20 18:35:27 +0530

    Update visualizeStreamlit.py

--- visualizeStreamlit.py
+++ visualizeStreamlit.py
@@ -8,6 +8,7 @@ import numpy as np
 import time
 import os
 from text_file import tonal_data_explanation, emotion_classifier
+from youtubeToMP3 import youtube_to_mp3
 from main_file import main_file_commands
 import asyncio
 import requests
@@ -20,9 +21,7 @@ with st.expander("See Explanation"):
     st.write(tonal_data_explanation)
 
 #st.write("You might have heard of text analysis? It is the process of analysing texts from a document. But texts can be highly deceivable. Simply a text isn't enough to understand what the speaker is trying to say. That is where Tonal Sentiment Analysis comes into the picture. Tonal Analysis is defined as using the speakers' various voice features such as loudness, pitch, frequency, etc., and quantifying them to retrieve insights which the speaker would exclusivley have. Tonal Analysis has a wide use case but we'll be focussing on how it can generate values that can give us an insight about what the company executives know about their company that others do not.")
-#youtube_url = st.text_input("Drop the link of the YouTube video you'd like to analyze", 'https://www.youtube.com/watch?v=72sz2zDQkqo')
 
-#youtube_code = (youtube_url[32:])
 
 #st.subheader("Analyzing the Audio from the YouTube Video")
 #st.caption("The audio analyzed was released by CNBC after Robert Reffkin layed off his employs for the 3rd time")
@@ -30,9 +29,18 @@ with st.expander("See Explanation"):
 with st.expander("ℹ️ Information about the Emotion Classifier"):
     st.info(emotion_classifier)
 
-#st.video('https://youtu.be/{}'.format(youtube_code)) 
+#emotion = 'Disgust'
+
+ 
+#youtube_url = st.text_input("Drop the link of the YouTube video you'd like to analyze", None)
+#youtube_code = (youtube_url[32:])
+#st.video('https://youtu.be/{}'.format(youtube_code))
 
 audio_file = st.file_uploader("Drop your MP3 file here", type=['MP3'], accept_multiple_files=False)
+
+with st.spinner('Waiting for you to upload the MP3 file...'):
+        time.sleep(10)
+        st.success('Done!')
 
 if audio_file is not None:
     # Save the MP3 file temporarily
@@ -40,9 +48,11 @@ if audio_file is not None:
         f.write(audio_file.getbuffer())
     
     # Get the absolute path of the uploaded file
-    audio_file_path = "/app/helios-dashboard-v2/temp.mp3" #os.path.abspath("temp.mp3")
+    audio_file_path = os.path.abspath("temp.mp3")
     st.write("File path:", audio_file_path)
     #main_file_commands(audio_file_path)
+#elif (youtube_code is not None):
+    #audio_file_path = youtube_to_mp3(youtube_code)
 else:
     with st.spinner('Waiting for you to upload the MP3 file...'):
         time.sleep(10)
@@ -55,23 +65,46 @@ else:
 
 #def emotion_classifier_fn(audio_file_path): # Create an async functon
 
+print('checkpoint 1')
 API_URL = "https://api-inference.huggingface.co/models/harshit345/xlsr-wav2vec-speech-emotion-recognition"
+print('checkpoint 2')
 headers = {"Authorization": "Bearer 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"}
+print('checkpoint 3')
 
 #filename = "temp.mp3"
 def query(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
+    try:
+        print('checkpoint 4')
+        with open(filename, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+    except Exception as e:
+        print('checkpoint 4')
+        print(e)
+        pass
 
-response = query(audio_file_path)
+    try:
+        return response.json()
+    except Exception as e:
+        print('Issue with Huggingface API')
+        pass
+
+time.sleep(20)
+try:
+    response = query("temp.mp3")
+except:
+    pass
 # Parse the JSON response
 # Convert dictionary to JSON string
 #json_str = json.dumps(response)
 
 # Create DataFrame from JSON string
-emotion_classifier_df = pd.DataFrame(response)
+print(response)
+try:
+    emotion_classifier_df = pd.DataFrame(response)
+except Exception as e:
+    emotion_classifier_df = pd.DataFrame(response, index=[0])
+#emotion_classifier_df.index = range(len(emotion_classifier_df))
 
 # Create a DataFrame from the JSON data
 #emotion_classifier_df = pd.DataFrame(data)
